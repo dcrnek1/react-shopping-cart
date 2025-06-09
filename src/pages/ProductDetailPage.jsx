5daa2f9660c7f4e547d3dcee8ff8e99bf210211c
dcrnek1 <dario.crnek@gmail.com>
2025-06-09 19:50:38 +0200

    Product details page responsiveness

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -174,10 +174,10 @@ export default function ProductDetailPage() {
                 </div>
               </div>
 
-              <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
+              <div className="mt-5 sm:gap-4 sm:items-center flex justify-end sm:justify-start sm:mt-8">
                 <button
                   onClick={() => addToCart(product)}
-                  className="text-white mt-4 sm:mt-0 bg-primary hover:bg-primary/70 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none flex items-center gap-3 justify-center"
+                  className="text-white mt-0 sm:mt-0 bg-primary hover:bg-primary/70 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none flex items-center gap-3 justify-center"
                 >
                   <ShoppingCartIcon size={20} weight="bold" />
                   Add to cart
